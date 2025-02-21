84498060df90c94456b6869db0b320cae77c862a
Azure Pipeline <pipeline@azure.com>
2025-02-21 10:18:41 +0100

    web.if_modified_since no overload

--- conf/web/schema/3/if_modified_since.sql
+++ conf/web/schema/3/if_modified_since.sql
@@ -1,26 +1,4 @@
-drop function if exists web.if_modified_since(text)
-;
-create or replace function web.if_modified_since
-    ( p_value text
-    )
-returns timestamptz
-language sql
-immutable
-as $function$
-    select to_timestamp
-        ( p_value
-        , 'Dy, DD Mon YYYY HH24:MI:SS TZ'
-        )
-$function$
-;
-
-comment on function web.if_modified_since(text) is $$
-Converts a string in the format of the If-Modified-Since header, e.g.
-'Wed, 19 Feb 2025 16:40:16 GMT', to a timestamp.
-$$;
-
-
-drop function if exists web.if_modified_since(jsonb)
+drop function if exists web.if_modified_since
 ;
 create or replace function web.if_modified_since
     ( p_header jsonb
@@ -29,19 +7,21 @@ returns timestamptz
 language sql
 immutable
 as $function$
-    -- Use the overloaded text-parameter function.
-    select web.if_modified_since ((
-        -- "value" is an array of strings; we extract the first element.
-        SELECT "value"->>0
-        -- "jsonb_each" returns a set of key-value pairs. This method allows us
-        -- to select the key case-insensitively.
-        FROM jsonb_each(p_header)
-        WHERE lower("key") = 'if-modified-since'
-    ))
+    select to_timestamp
+        ( (
+            -- "value" is an array of strings; we extract the first element.
+            SELECT "value"->>0
+            -- "jsonb_each" returns a set of key-value pairs. This method allows
+            -- us to select the key case-insensitively.
+            FROM jsonb_each(p_header)
+            WHERE lower("key") = 'if-modified-since'
+          )
+        , 'Dy, DD Mon YYYY HH24:MI:SS TZ'
+        )
 $function$
 ;
 
-comment on function web.if_modified_since(jsonb) is $$
+comment on function web.if_modified_since is $$
 Converts the value of the If-Modified-Since header, e.g.
 'Wed, 19 Feb 2025 16:40:16 GMT', to a timestamp.
 $$;
